c4b89da427c53a5196aed75dffaa470fac33dd9f
rustynailor <russell@scapegrace.co.uk>
2016-02-03 06:59:34 +0000

    feat: draft detailmovie layout added, Movie class made parcelable

--- app/src/main/java/uk/co/rustynailor/android/popularmovies/Movie.java
+++ app/src/main/java/uk/co/rustynailor/android/popularmovies/Movie.java
@@ -1,10 +1,13 @@
 package uk.co.rustynailor.android.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * This models a single movie
  * Created by russellhicks on 31/01/16.
  */
-public class Movie {
+public class Movie implements Parcelable{
 
     private String mTitle;
     private String mPosterPath;
@@ -12,9 +15,31 @@ public class Movie {
     private String mVoteAverage;
     private String mMovieDescription;
 
+    //constructors added for parcelable
+    public Movie(){
+    }
+
+    public Movie(Parcel source){
+        readFromParcel(source);
+    }
+
     public String getTitle() {
         return mTitle;
     }
+
+    public static final Parcelable.Creator<Movie> CREATOR =
+            new Parcelable.Creator<Movie>(){
+
+                @Override
+                public Movie createFromParcel(Parcel source) {
+                    return new Movie(source);
+                }
+
+                @Override
+                public Movie[] newArray(int size) {
+                    return new Movie[size];
+                }
+            };
 
     public void setTitle(String mTitle) {
         this.mTitle = mTitle;
@@ -50,5 +75,27 @@ public class Movie {
 
     public void setMovieDescription(String mMovieDescription) {
         this.mMovieDescription = mMovieDescription;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0; //unused
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeString(mPosterPath);
+        dest.writeString(mReleaseDate);
+        dest.writeString(mVoteAverage);
+        dest.writeString(mMovieDescription);
+    }
+
+    public void readFromParcel(Parcel source){
+        mTitle = source.readString();
+        mPosterPath = source.readString();
+        mReleaseDate = source.readString();
+        mVoteAverage = source.readString();
+        mMovieDescription = source.readString();
     }
 }
