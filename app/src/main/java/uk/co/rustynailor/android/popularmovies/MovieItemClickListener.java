ed21b3c9a640579390786e0201ccbbfe11bd3596
rustynailor <russell@scapegrace.co.uk>
2016-02-04 07:01:08 +0000

    feat: onClick Listener added to recyclerview holder to link detail screen

---
+++ app/src/main/java/uk/co/rustynailor/android/popularmovies/MovieItemClickListener.java
@@ -0,0 +1,10 @@
+package uk.co.rustynailor.android.popularmovies;
+
+import android.view.View;
+
+/**
+ * Created by russellhicks on 04/02/16.
+ */
+public interface MovieItemClickListener {
+        public void onItemClick(View v, int position);
+}
