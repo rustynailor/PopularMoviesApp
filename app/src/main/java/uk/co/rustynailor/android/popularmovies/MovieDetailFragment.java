1e397f4ce6312c46b983611cc69506cd8eac1aa4
rustynailor <russell@scapegrace.co.uk>
2016-02-06 07:15:09 +0000

    feat: movie detail view now retrieves poster

--- app/src/main/java/uk/co/rustynailor/android/popularmovies/MovieDetailFragment.java
+++ app/src/main/java/uk/co/rustynailor/android/popularmovies/MovieDetailFragment.java
@@ -1,11 +1,16 @@
 package uk.co.rustynailor.android.popularmovies;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import uk.co.rustynailor.android.popularmovies.R;
 
@@ -19,6 +24,7 @@ public class MovieDetailFragment extends Fragment {
     private TextView mLength;
     private TextView mRating;
     private TextView mSynopsis;
+    private ImageView mPoster;
 
 
     public MovieDetailFragment() {
@@ -48,6 +54,49 @@ public class MovieDetailFragment extends Fragment {
 
         mSynopsis = (TextView) view.findViewById(R.id.synopsis);
         mSynopsis.setText(mMovie.getMovieDescription());
+
+        mPoster = (ImageView) view.findViewById(R.id.poster);
+
+        //Build URL to download image
+        Uri.Builder builder = new Uri.Builder();
+
+        final String MOVIE_API_IMAGE_AUTHORITY = "image.tmdb.org";
+        final String MOVIE_API_PATH_1 = "t";
+        final String MOVIE_API_PATH_2 = "p";
+        final String MOVIE_API_PATH_3 = "w185";
+
+        //Trim leading slash from path
+        String posterPath = mMovie.getPosterPath().substring(1);
+
+        builder.scheme("http")
+                .authority(MOVIE_API_IMAGE_AUTHORITY)
+                .appendPath(MOVIE_API_PATH_1)
+                .appendPath(MOVIE_API_PATH_2)
+                .appendPath(MOVIE_API_PATH_3)
+                .appendPath(posterPath);
+
+
+        String url = builder.build().toString();
+        Log.d("MOVIE POSTER VIEW", url);
+
+        int width= getActivity().getResources()
+                .getDisplayMetrics()
+                .widthPixels;
+
+        int numColumns = 2;
+
+        //cinema poster ratio, as found here
+        //http://www.imdb.com/help/show_leaf?photosspecs
+        double posterRatio = 1.48;
+
+
+        Picasso.with(getActivity())
+                .load(url)
+                .centerCrop()
+                .resize(width / numColumns, (int) Math.round((width / numColumns) * posterRatio))
+                .into(mPoster);
+
+
 
         return view;
     }
